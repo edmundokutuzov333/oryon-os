67b01ba4237cadef7a27d1fa3672e9a9b7f2374b
edmundokutuzov333 <contact@edmundokutuzov.art>
2026-09-16 15:40:20 +0200

    feat(db): export docs files repository

--- packages/db/src/repositories/index.ts
+++ packages/db/src/repositories/index.ts
@@ -1,3 +1,4 @@
+export { DocsFilesRepository } from "./docs-files.repository.js";
 export { GraphRepository } from "./graph.repository.js";
 export type { GraphEdgeRecord, GraphNodeRecord, GraphTraverseResult } from "./graph.repository.js";
 export { IdentityRepository } from "./identity.repository.js";
