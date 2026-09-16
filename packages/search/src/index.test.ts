ea2c3dc49db81d0fdff997ddc7e521d9eb3a6b7a
edmundokutuzov333 <contact@edmundokutuzov.art>
2026-09-16 11:06:08 +0200

    test(search): add bootstrap unit test

---
+++ packages/search/src/index.test.ts
@@ -0,0 +1,6 @@
+import { describe, expect, it } from "vitest";
+import { ORYON_SEARCH_VERSION } from "./index";
+
+describe("search bootstrap", () => {
+  it("exports the search version", () => expect(ORYON_SEARCH_VERSION).toBe("0.1.0"));
+});
