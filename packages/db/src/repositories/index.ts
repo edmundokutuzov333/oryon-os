f15259263e33ece10eb3d922cac0e0a2f561a58b
edmundokutuzov333 <contact@edmundokutuzov.art>
2026-09-16 15:27:49 +0200

    feat(db): export work experience repository

--- packages/db/src/repositories/index.ts
+++ packages/db/src/repositories/index.ts
@@ -3,6 +3,7 @@ export type { GraphEdgeRecord, GraphNodeRecord, GraphTraverseResult } from "./gr
 export { IdentityRepository } from "./identity.repository.js";
 export { PermissionRepository } from "./permission.repository.js";
 export type { PermissionSnapshot, ExternalExposureRow } from "./permission.repository.js";
+export { WorkExperienceRepository } from "./work-experience.repository.js";
 export { WorkObjectRepository } from "./work-object.repository.js";
 export type { WorkObjectListInput } from "./work-object.repository.js";
 export { appendDomainEvent } from "../outbox.js";
