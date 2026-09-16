d890d56f3363c7a3e890bb5ed17693ff2b248863
edmundokutuzov333 <contact@edmundokutuzov.art>
2026-09-16 14:10:42 +0200

    feat(permissions): export permission repository

--- packages/db/src/repositories/index.ts
+++ packages/db/src/repositories/index.ts
@@ -1,4 +1,6 @@
 export { IdentityRepository } from "./identity.repository.js";
+export { PermissionRepository } from "./permission.repository.js";
+export type { PermissionSnapshot, ExternalExposureRow } from "./permission.repository.js";
 export { WorkObjectRepository } from "./work-object.repository.js";
 export type { WorkObjectListInput } from "./work-object.repository.js";
 export { appendDomainEvent } from "../outbox.js";
