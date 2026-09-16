e0534b6470ca7aa84588c1e31219974468b131a9
edmundokutuzov333 <contact@edmundokutuzov.art>
2026-09-16 15:24:39 +0200

    refactor(graph): use shared app shell main

--- apps/web/src/app/os/graph/page.tsx
+++ apps/web/src/app/os/graph/page.tsx
@@ -2,9 +2,5 @@ import "./graph.module.css";
 import { GraphStudio } from "./GraphStudio";
 
 export default function GraphPage() {
-	return (
-		<main className="os-shell">
-			<GraphStudio />
-		</main>
-	);
+  return <GraphStudio />;
 }
