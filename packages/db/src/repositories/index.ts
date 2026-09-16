74b602ba843f084368564645254b744c849d5fa8
edmundokutuzov333 <contact@edmundokutuzov.art>
2026-09-16 13:56:55 +0200

    feat(identity): export identity repository

--- packages/db/src/repositories/index.ts
+++ packages/db/src/repositories/index.ts
@@ -1,3 +1,4 @@
+export { IdentityRepository } from "./identity.repository.js";
 export { WorkObjectRepository } from "./work-object.repository.js";
 export type { WorkObjectListInput } from "./work-object.repository.js";
 export { appendDomainEvent } from "../outbox.js";
